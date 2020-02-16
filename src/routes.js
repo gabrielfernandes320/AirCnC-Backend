607ba77e0a4b9ff8584c10e38aca59a730f48f5c
Gabriel <gabrielfernandesfg@hotmail.com>
2020-02-16 20:49:51 -0300

    Creation of API

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const multer = require("multer");
+const uploadConfig = require("./config/upload.js");
+
 const sessionController = require("./controllers/SessionController");
+const spotController = require("./controllers/SpotController");
+const dashboardController = require("./controllers/DashboardController");
+const bookingController = require("./controllers/BookingController");
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
 routes.post("/sessions", sessionController.store);
+
+routes.post("/spots", upload.single("thumbnail"), spotController.store);
+routes.get("/spots", spotController.index);
+routes.post("/spots/:spot_id/bookings", bookingController.store);
+
+routes.get("/dashboard", dashboardController.show);
 
 module.exports = routes;
